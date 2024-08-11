07560202e8e784b32bd67d8954832f70d0a1795a
Balazs Gazdag <gazdagbal@gmail.com>
2024-08-11 20:06:37 +0200

    Full width added to bg

--- src/sections/FunFacts.jsx
+++ src/sections/FunFacts.jsx
@@ -2,7 +2,7 @@ import Button from '../components/Button'
 
 const FunFacts = () => {
   return (
-    <div className='bg-[#2C2C2C] text-white flex flex-col gap-10 items-center py-10'>
+    <div className='bg-[#2C2C2C] text-white flex flex-col gap-10 items-center py-10 w-full'>
         <div className='text-center'>
             <h2 className='text-[23px] font-[700] mb-4'>Random facts about me</h2>
             <p className='text-[18px]'>&quot;I&apos;m currently 25 years old.&quot;</p>
