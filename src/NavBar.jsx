870bdc600a9b24eccd5380da1c3bc1a8f32970fd
Balazs Gazdag <gazdagbal@gmail.com>
2024-08-11 17:35:43 +0200

    Mobile Navbar Finished

---
+++ src/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import gbLogo from "./assets/gb-logo-text.svg"
+import "./NavBar.css"
+
+const NavBar = () => {
+  return (
+    <nav className='flex w-100 justify-between p-5 items-center'>
+      <img src={gbLogo} alt="" style={{height: "50px"}}/>
+      <div className="hamburger">
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
