bc4662934145ea3a794507cf8df34c1870c4f2fa
Balazs Gazdag <gazdagbal@gmail.com>
2024-08-12 20:40:24 +0200

    Added higher z-index

--- src/sections/NavBar.jsx
+++ src/sections/NavBar.jsx
@@ -3,7 +3,7 @@ import "./NavBar.css"
 
 const NavBar = () => {
   return (
-    <nav className='flex w-full justify-between px-5 py-3 items-center fixed bg-white'>
+    <nav className='flex w-full justify-between px-5 py-3 items-center fixed bg-white z-10'>
       <img src={gbLogo} alt="" style={{height: "50px"}}/>
       <div className="hamburger">
         <div className="bar"></div>
