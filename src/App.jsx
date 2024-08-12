b7ef3b667a0c7db3ab4c58ee5ce1cd021cb9b1ed
Balazs Gazdag <gazdagbal@gmail.com>
2024-08-12 20:41:01 +0200

    About Details section built

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import FunFacts from './sections/FunFacts'
 import Hero from './sections/Hero'
 import NavBar from './sections/NavBar'
 import Video from './sections/Video'
+import Details from './components/Details'
+import Skills from './components/Skills'
 
 
 function App() {
@@ -20,7 +22,17 @@ function App() {
     <h2 className='text-white u-h2 '>Get to know me</h2>
     <p className='u-body text-white text-center px-[24px] text-pretty mb-10'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod totam ipsum labore distinctio harum. Error amet dignissimos hic aliquid labore deleniti molestiae rem sit id dolore tempora vitae eveniet illo voluptatibus nesciunt, iusto sint laudantium.</p>
 
+    <div >
+      <Details detailTitle={"AGE"} detailBody={"25 Years"}/>
+      <Details detailTitle={"JOB TITLE"} detailBody={"Fullstack Engineer & Graphic Designer"}/>
+      <Details detailTitle={"NATOINALITY"} detailBody={"Hungarian"}/>
+      <Details detailTitle={"LANGUAGES"} detailBody={"Hungarian, English, German"}/>
+    </div>
 
+    <section>
+      <h2 className='u-h2 text-white'>My Skills</h2>
+      <Skills/>
+    </section>
     </div>
   )
 }
