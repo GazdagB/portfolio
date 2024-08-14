b162b428e831b2192cd9f66aa9a66992e57fa3ac
Gazdag Balázs <gazdagbal@gmail.com>
2024-08-14 19:48:27 +0200

    Skill icons tidy up

--- src/components/IconSkill.jsx
+++ src/components/IconSkill.jsx
@@ -2,10 +2,9 @@
 const IconSkill = ({icon, level, skillName}) => {
   return (
     <div className='flex flex-col items-center justify-center h-[140px]'>
-        <img className="w-[70px]" src={icon} alt="" />
-        <div className="progress-bar h-[6px] w-[80px] bg-[#D9D9D9] rounded-full items-center">
+        <img className="h-[70px] mb-3" src={icon} alt="" />
+        <div className="progress-bar h-[6px] w-[80px] mb-2 bg-[#D9D9D9] rounded-full items-center">
             <div style={{width: `${level}%`}} className={`progress rounded-full bg-[#EF479B] h-[6px]`} ></div>
-            
         </div>
         <p className='text-[18px] font-semibold text-white'>{skillName}</p>
     </div>
